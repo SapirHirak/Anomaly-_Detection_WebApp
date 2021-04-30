ae56b612d17cc07aa343a28e1ecc2697928f6808
SapirHirak <sapiroosh1h@gmail.com>
2021-05-01 00:13:58 +0300

    graph and table basics

--- detection-webapp/src/DragDrop.js
+++ detection-webapp/src/DragDrop.js
@@ -1,17 +1,26 @@
-function DragDrop() {
 
-    /*function handleEnter(event) {
-        if (event.code === 'Enter') {
-            addItem(event.target.value);
-        }
-    }*/
+import React from 'react';
+import { FileDrop } from 'react-file-drop';
 
+export const DragDrop = () => {
+    const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
     return (
-
         <div>
-            Im DragDrop
+            <h1>React File Drop demo</h1>
+            <div style={styles}>
+                <FileDrop
+                    onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
+                    onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
+                    onFrameDrop={(event) => console.log('onFrameDrop', event)}
+                    onDragOver={(event) => console.log('onDragOver', event)}
+                    onDragLeave={(event) => console.log('onDragLeave', event)}
+                    onDrop={(files, event) => console.log('onDrop!', files, event)}
+                >
+                    Drop some files here!
+                </FileDrop>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default DragDrop;
