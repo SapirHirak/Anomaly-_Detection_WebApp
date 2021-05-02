49f1ad0da172e9e8d7062c0694098287ae6e558f
ArielDrellich <arieldrellich@gmail.com>
2021-05-02 10:17:19 +0300

    drag drop first trial, no css yet

--- detection-webapp/src/DragDrop.js
+++ detection-webapp/src/DragDrop.js
@@ -1,3 +1,7 @@
+// https://www.npmjs.com/package/react-dropzone
+import React, {useCallback} from 'react'
+import Dropzone from 'react-dropzone'
+import {useDropzone} from 'react-dropzone'
 function DragDrop() {
 
     /*function handleEnter(event) {
@@ -5,13 +9,48 @@ function DragDrop() {
             addItem(event.target.value);
         }
     }*/
-
+    
+    const onDrop = useCallback((acceptedFiles) => {
+      acceptedFiles.forEach((file) => {
+        const reader = new FileReader()
+  
+        reader.onabort = () => console.log('file reading was aborted')
+        reader.onerror = () => console.log('file reading has failed')
+        reader.onload = () => {
+        // Do whatever you want with the file contents
+          const binaryStr = reader.result
+          console.log(binaryStr)
+        }
+        reader.readAsArrayBuffer(file)
+      })
+      
+    }, [])
+    const {getRootProps, getInputProps} = useDropzone({onDrop})
+  
     return (
-
-        <div>
-            Im DragDrop
-        </div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
     )
+
+    // return (
+        
+    //     // <div>
+    //     //     Im DragDrop
+    //     // </div>
+    //     <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+    //       {({getRootProps, getInputProps}) => (
+    //         <section>
+    //           <div {...getRootProps()}>
+    //             <input {...getInputProps()} />
+    //             <p>Drag learn csv here</p>
+    //           </div>
+    //         </section>
+    //       )}
+    //     </Dropzone>
+        
+    // )
 }
 
 export default DragDrop;
