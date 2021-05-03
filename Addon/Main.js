cf6c85ce1d40658474b19e9a9618d60e17c4ede1
ArielDrellich <arieldrellich@gmail.com>
2021-05-03 19:50:10 +0300

    output detect to proper json

--- Addon/Main.js
+++ Addon/Main.js
@@ -1,5 +1,12 @@
 const learn = require('./build/Release/learnNormal')
 
+let clientId = 12345
+
+// send path for learning
+console.log(learn.learnN("anomalyTrain.csv"))
 // send path, followed by client id
-console.log(learn.learnN("anomalyTrain.csv",76))
-learn.detect("anomalyTest.csv", 12345432)
+learn.detect("anomalyTest.csv", clientId)
+
+//print json to make sure it's valid
+const a = require(`./Anomalies-${clientId}.json`)
+console.log(a)
--- Addon/learnNormal.cpp
+++ Addon/learnNormal.cpp
@@ -25,24 +25,30 @@ namespace learnNormal {
     SimpleAnomalyDetector sad;
 
     void Learn(const FunctionCallbackInfo<Value>&args) {
-        Isolate* isolate = args.GetIsolate();
         TimeSeries tsTrain(*v8::String::Utf8Value(args[0])); 
 	    sad.learnNormal(tsTrain);
         args.GetReturnValue().Set(args[1]->NumberValue());;
     }
 
     void Detect(const FunctionCallbackInfo<Value>&args) {
+
 	    TimeSeries tsTest(*v8::String::Utf8Value(args[0]));
 	    std::vector<AnomalyReport> anomalyReportVec = sad.detect(tsTest);
 
 	    ofstream file("Anomalies-" + to_string((int)args[1]->NumberValue()) + ".json");
 	    if (file.is_open()) {
+            file << "{" << std::endl;
 	    	for (int i = 0; i < anomalyReportVec.size(); i++) {
-	    		file << anomalyReportVec[i].timeStep<< "," << anomalyReportVec[i].description << std::endl;
+
+	    		file << "\"Anonamly" << i << "\": {" << std::endl << "\"Description\":\"" << 
+                anomalyReportVec[i].description << "\"," << std::endl 
+                << "\"timestep\":" << anomalyReportVec[i].timeStep;
+                    (i == anomalyReportVec.size() - 1) ? file << std::endl << "}" << std::endl : file << std::endl << "}," << std::endl;
+
 	    	}
+            file << "}" << std::endl;
 	    	file.close();
 	    }
-        
         args.GetReturnValue().Set(args[1]->NumberValue());
     }
 
