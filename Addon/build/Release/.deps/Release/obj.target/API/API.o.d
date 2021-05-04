59f7ae3119c1ffc2965f4da6705f59bc2521219a
Aviv <avivrahamim95@gmail.com>
2021-05-04 22:33:44 +0300

    installs

--- Addon/build/Release/.deps/Release/obj.target/API/API.o.d
+++ Addon/build/Release/.deps/Release/obj.target/API/API.o.d
@@ -1,15 +1,14 @@
-cmd_Release/obj.target/API/API.o := g++ -o Release/obj.target/API/API.o ../API.cpp '-DNODE_GYP_MODULE_NAME=API' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.cache/node-gyp/10.19.0/include/node -I/root/.cache/node-gyp/10.19.0/src -I/root/.cache/node-gyp/10.19.0/deps/openssl/config -I/root/.cache/node-gyp/10.19.0/deps/openssl/openssl/include -I/root/.cache/node-gyp/10.19.0/deps/uv/include -I/root/.cache/node-gyp/10.19.0/deps/zlib -I/root/.cache/node-gyp/10.19.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/API/API.o.d.raw   -c
+cmd_Release/obj.target/API/API.o := g++ -o Release/obj.target/API/API.o ../API.cpp '-DNODE_GYP_MODULE_NAME=API' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/include/node -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/openssl/config -I/usr/include/nodejs/deps/openssl/openssl/include -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/zlib -I/usr/include/nodejs/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/API/API.o.d.raw   -c
 Release/obj.target/API/API.o: ../API.cpp ../timeseries.cpp \
  ../timeseries.h ../SimpleAnomalyDetector.cpp ../SimpleAnomalyDetector.h \
  ../anomaly_detection_util.h ../AnomalyDetector.h ../minCircle.h \
  ../HybridAnomalyDetector.cpp ../HybridAnomalyDetector.h \
  ../anomaly_detection_util.cpp ../minCircle.cpp \
- /root/.cache/node-gyp/10.19.0/include/node/node.h \
- /root/.cache/node-gyp/10.19.0/include/node/v8.h \
- /root/.cache/node-gyp/10.19.0/include/node/v8-version.h \
- /root/.cache/node-gyp/10.19.0/include/node/v8config.h \
- /root/.cache/node-gyp/10.19.0/include/node/v8-platform.h \
- /root/.cache/node-gyp/10.19.0/include/node/node_version.h
+ /usr/include/nodejs/src/node.h /usr/include/nodejs/deps/v8/include/v8.h \
+ /usr/include/nodejs/deps/v8/include/v8-version.h \
+ /usr/include/nodejs/deps/v8/include/v8config.h \
+ /usr/include/nodejs/deps/v8/include/v8-platform.h \
+ /usr/include/nodejs/src/node_version.h
 ../API.cpp:
 ../timeseries.cpp:
 ../timeseries.h:
@@ -22,9 +21,9 @@ Release/obj.target/API/API.o: ../API.cpp ../timeseries.cpp \
 ../HybridAnomalyDetector.h:
 ../anomaly_detection_util.cpp:
 ../minCircle.cpp:
-/root/.cache/node-gyp/10.19.0/include/node/node.h:
-/root/.cache/node-gyp/10.19.0/include/node/v8.h:
-/root/.cache/node-gyp/10.19.0/include/node/v8-version.h:
-/root/.cache/node-gyp/10.19.0/include/node/v8config.h:
-/root/.cache/node-gyp/10.19.0/include/node/v8-platform.h:
-/root/.cache/node-gyp/10.19.0/include/node/node_version.h:
+/usr/include/nodejs/src/node.h:
+/usr/include/nodejs/deps/v8/include/v8.h:
+/usr/include/nodejs/deps/v8/include/v8-version.h:
+/usr/include/nodejs/deps/v8/include/v8config.h:
+/usr/include/nodejs/deps/v8/include/v8-platform.h:
+/usr/include/nodejs/src/node_version.h:
