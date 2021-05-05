bc85610962391dc76e7d49251eb752cd667c1fcd
Aviv <avivrahamim95@gmail.com>
2021-05-05 18:09:51 +0300

    server template

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,43 @@
-//require('dotenv').config()
+const express = require('express');
+
+// express app
+const server = express();
+
+// listen to requests
+server.listen(9876);
+
+// get for every URL that we want to listen to
+/****************
+ * need to put this in the main directory because of the paths
+ **************/
+server.get('/', (req, res) => { // root domain listen
+    //res.send('<p>home page</p>');
+    // sends the client an html file
+    res.sendFile('/index.html', {root: __dirname});
+});
+
+server.get('/api', (req, res) => {
+    //res.send('<p>api page</p>');
+});
+
+server.get('/api/model', (req, res) => {
+    //res.send('<p>model page</p>');
+});
+
+// redirects
+server.get('/api-us', (req, res) => {
+    res.redirect('/api');
+});
+
+// 404 page
+// because it has no url it goes for EVERY url (happens only if there was no match above)
+server.use((req, res) => {
+    res.status(404).sendFile('/404.html', {root: __dirname});
+});
+
+
+// old stuff probably delete **********************************************
+/*//require('dotenv').config()
 
 const express = require('express')
 const app = express()
@@ -28,4 +67,4 @@ mongoose.connect('mongodb://localhost/CRM', { useNewUrlParser: true }).then(() =
 
 
     app.listen(port, () => console.log(`Running server on port ${port}`))
-})
+})*/
